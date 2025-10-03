363c668aa7010e3cb811aa8f3990f542bf670bb5
Ruslan0019 <Ruslan001925@gmail.com>
2025-10-03 19:56:54 +0300

    favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,21 +4,45 @@ import Header from "@/components/Header/Header";
 import { NextIntlClientProvider } from "next-intl";
 export const metadata = {
   charset: "UTF-8",
-  icons: {
-    icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon.ico", sizes: "any" },
-    ],
-    apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-    ],
-  },
-  manifest: "/site.webmanifest",
 };
 export default function RootLayout({ children }) {
   return (
     <html>
+      <head>
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/icon0.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon1.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/web-app-manifest-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/web-app-manifest-512x512.png"
+        />
+
+        {/* apple / touch */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="cubefreestyle" />
+
+        {/* PWA / manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0a0a0a" />
+
+        {/* optional helpers */}
+        <link rel="mask-icon" href="/icon0.svg" color="#0a0a0a" />
+        <meta name="msapplication-TileColor" content="#0a0a0a" />
+      </head>
       <body className="flex flex-col min-h-screen ">
         <NextIntlClientProvider>
           <Header />
